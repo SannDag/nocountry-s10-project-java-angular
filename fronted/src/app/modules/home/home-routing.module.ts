4f088d54c4db6f4531a380feacd284bd215f14eb
mafer23 <mafer18199420@gmail.com>
2023-08-19 20:37:25 -0500

    cambios de navbar

--- fronted/src/app/modules/home/home-routing.module.ts
+++ fronted/src/app/modules/home/home-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home.page';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+
 
 const routes: Routes = [
   {
