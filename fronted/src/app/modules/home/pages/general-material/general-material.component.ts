aef1f7080f4e7c6fd5f0d4583c6680ddb30dddb3
Abel3581 <marzoa3581@gmail.com>
2023-08-31 16:54:51 -0300

    agregado actualizar token y cerrar sesion en otros componentes

--- fronted/src/app/modules/home/pages/general-material/general-material.component.ts
+++ fronted/src/app/modules/home/pages/general-material/general-material.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { InactivityService } from 'src/app/services/inactivity.service';
+import { TokenService } from 'src/app/services/token.service';
 
 
 @Component({
@@ -6,6 +10,44 @@ import { Component } from '@angular/core';
   templateUrl: './general-material.component.html',
   styleUrls: ['./general-material.component.scss']
 })
-export class GeneralMaterialComponent {
+export class GeneralMaterialComponent implements OnInit{
+
+  private updateIntervalMinutes = 3;
+  private updateIntervalMillis = this.updateIntervalMinutes * 60 * 1000;
+  private userActivitySubscription: Subscription | undefined;
+
+  constructor(private tokenService: TokenService, private authService: AuthService,
+    private inactivityService: InactivityService){}
+
+  ngOnInit(): void {
+    if (this.tokenService.isLoggued()) {
+      // Actualizar token cada 3 minutos
+      this.userActivitySubscription = interval(this.updateIntervalMillis).subscribe(() => {
+        this.updateToken();
+      });
+      this.inactivityService.startInactivityTimer();
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Cancelar la suscripción cuando se destruye el componente
+    if (this.userActivitySubscription) {
+      this.userActivitySubscription.unsubscribe();
+    }
+  }
+
+  private updateToken(): void {
+    this.authService.getCurrentSession().subscribe(
+      updatedSession => {
+        console.log("Token viejo: " + this.tokenService.getToken())
+        this.tokenService.setToken(updatedSession.token);
+        console.log("Token nuevo:" + this.tokenService.getToken());
+      },
+      error => {
+        console.error('Error al actualizar el token:', error);
+      }
+
+    )
+  }
 
 }
--- fronted/src/app/modules/home/pages/informa-laboral/informa-laboral.component.ts
+++ fronted/src/app/modules/home/pages/informa-laboral/informa-laboral.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+import { InactivityService } from 'src/app/services/inactivity.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-informa-laboral',
   templateUrl: './informa-laboral.component.html',
   styleUrls: ['./informa-laboral.component.scss']
 })
-export class InformaLaboralComponent {
+export class InformaLaboralComponent implements OnInit{
 
+  private updateIntervalMinutes = 3;
+  private updateIntervalMillis = this.updateIntervalMinutes * 60 * 1000;
+  private userActivitySubscription: Subscription | undefined;
+
+  constructor(private tokenService: TokenService, private authService: AuthService,
+    private inactivityService: InactivityService){}
+
+  ngOnInit(): void {
+    if (this.tokenService.isLoggued()) {
+      // Actualizar token cada 3 minutos
+      this.userActivitySubscription = interval(this.updateIntervalMillis).subscribe(() => {
+        this.updateToken();
+      });
+      this.inactivityService.startInactivityTimer();
+    }
+  }
+
+  ngOnDestroy(): void {
+    // Cancelar la suscripción cuando se destruye el componente
+    if (this.userActivitySubscription) {
+      this.userActivitySubscription.unsubscribe();
+    }
+  }
+
+  private updateToken(): void {
+    this.authService.getCurrentSession().subscribe(
+      updatedSession => {
+        console.log("Token viejo: " + this.tokenService.getToken())
+        this.tokenService.setToken(updatedSession.token);
+        console.log("Token nuevo:" + this.tokenService.getToken());
+      },
+      error => {
+        console.error('Error al actualizar el token:', error);
+      }
+
+    )
+  }
 }
