7627c7e9db899d25280d71b5d65077a59575565c
Daniel Contreras <122397709+Dcopito@users.noreply.github.com>
2023-08-31 08:22:01 -0500

    Fix(Null Request)

--- backend/loans-application/src/main/java/s1014ftjavaangular/loansapplication/infrastructure/persistence/repository/guarantor/GuarantorJpaRepositoryAdapter.java
+++ backend/loans-application/src/main/java/s1014ftjavaangular/loansapplication/infrastructure/persistence/repository/guarantor/GuarantorJpaRepositoryAdapter.java
@@ -25,6 +25,17 @@ public class GuarantorJpaRepositoryAdapter implements GuarantorRepository {
         if (!isGuarantorExists) throw new RuntimeException("Guarantor does not exists");
 
         var entity = guarantorJpaRepository.findById(dto.getLoanApplicationId()).get();
+            
+        if (dto.getLoanApplicationId() == null) entity.setLoanApplicationId(entity.getLoanApplicationId());
+        if (dto.getName() == null) entity.setName(entity.getName());
+        if (dto.getLastname() == null) entity.setLastname(entity.getLastname());
+        if (dto.getIdentificationType() != null) entity.setIdentificationType(entity.getIdentificationType());
+        if (dto.getIdentification() == null) entity.setIdentification(entity.getIdentification());
+        if (dto.getCity() == null) entity.setCity(entity.getCity());
+        if (dto.getState() == null) entity.setState(entity.getState());
+        if (dto.getAddress() == null) entity.setAddress(entity.getAddress());
+        if (dto.getZipcode() == null) entity.setZipcode(entity.getZipcode());
+        if (dto.getPhone() == null) entity.setPhone(entity.getPhone());
 
         if (dto.getLoanApplicationId() != null) entity.setLoanApplicationId(dto.getLoanApplicationId());
         if (dto.getName() != null) entity.setName(dto.getName());
