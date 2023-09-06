829d0841f4e8c6a5d73c61795a7de2b4f461060e
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-09-06 17:01:22 -0400

    Implementing the amortization tables Outstanding Interest Monthly.

--- backend/loan/src/main/java/s1014ftjavaangular/loan/domain/services/OutstandingInterestStrategy.java
+++ backend/loan/src/main/java/s1014ftjavaangular/loan/domain/services/OutstandingInterestStrategy.java
@@ -12,40 +12,26 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
-import static s1014ftjavaangular.loan.domain.util.UtilitiesCalculations.NUMBER_OF_MONTHS;
-import static s1014ftjavaangular.loan.domain.util.UtilitiesCalculations.calculateNextLoanDate;
+import static s1014ftjavaangular.loan.domain.util.UtilitiesCalculations.*;
 
 public class OutstandingInterestStrategy extends AmortizationStrategy{
 
-    public Double calculateCapital(Double capital, Double annualInterest, LocalDate currentDate, Integer numberInstallments, FrequencyPayment frequencyPayment) {
-
-        if (FrequencyPayment.MONTHLY.getValue() == frequencyPayment.getValue()) {
-            BigDecimal annualInterestRate = BigDecimal.valueOf(annualInterest);
-            BigDecimal numerator = annualInterestRate.multiply(BigDecimal.valueOf(numberInstallments / NUMBER_OF_MONTHS)).add(BigDecimal.ONE);
-            BigDecimal denominator = BigDecimal.valueOf(Math.pow(numerator.doubleValue(), numberInstallments));
-            BigDecimal result = BigDecimal.valueOf(capital).multiply(numerator.divide(denominator, 10, RoundingMode.HALF_UP));
-            return result.doubleValue();
-        }
-        return null;
-
+    public Double calculateCapital(Double capital, Double annualInterest, LocalDate currentDate, Integer numberInstallments, FrequencyPayment frequencyPayment){
+        return formatDecimal(capital / numberInstallments);
     }
 
-
-    public Double calculateInterest(Double capital, Double annualInterest, Integer numberInstallments, FrequencyPayment frequencyPayment) {
-        BigDecimal capitalInstallment = BigDecimal.valueOf(calculateCapital(capital, annualInterest, null, numberInstallments, null));
-        BigDecimal interest = capitalInstallment.subtract(BigDecimal.valueOf(capital));
-        return interest.doubleValue();
+    public Double calculateInterest(Double capital, Double annualInterest, LocalDate currentDate, Integer numberInstallments, FrequencyPayment frequencyPayment) {
+        Double interest = (annualInterest / NUMBER_OF_MONTHS)/HUNDRED_PERCENT;
+        Double amountInterest = capital * interest;
+        return formatDecimal(amountInterest);
     }
 
     @Override
     public List<AmortizationSchedule> generateTable(Loan model) {
-        //Numero de cuotas
         Integer numbersInstallments = model.getNumberInstallments();
-
         List<AmortizationSchedule> amortizationSchedules = new LinkedList<>();
 
         for (int i = 1; i <= numbersInstallments; i++) {
-
             var currentPaymentDate = (i == 1)
                     ? model.getStartAt()
                     : calculateNextLoanDate(
@@ -54,31 +40,30 @@ public class OutstandingInterestStrategy extends AmortizationStrategy{
                     model.getFrequencyPayment().getValue(),
                     model.getFrequencyPayment()
             );
-            var capitalInstallment = calculateCapital(
-                    model.getAmountApproved(),
+
+            var interestInstallment = calculateInterest(
+                    (i == 1) ? model.getAmountApproved() : (amortizationSchedules.get(i - 2).getInterest() + model.getAmountApproved()),
                     model.getInterestRateLoan().getAnnualPercentage(),
-                    model.getStartAt(),
+                    currentPaymentDate,
+                    numbersInstallments,
+                    model.getFrequencyPayment()
+            );
+            var capitalInstallment = calculateCapital(
+                    (i == 1) ? model.getAmountApproved() : amortizationSchedules.get(i - 2).getCapitalBalance(),
+                    model.getInterestRateLoan().getAnnualPercentage(),
+                    currentPaymentDate,
                     numbersInstallments,
                     model.getFrequencyPayment()
             );
 
-            var capitalBalance = formatDecimal(
-                    (i == 1)
-                            ? model.getAmountApproved() - capitalInstallment
-                            : amortizationSchedules.get(i - 2).getCapitalBalance() - capitalInstallment
-            );
+            var capitalBalance = (i == 1)
+                    ? model.getAmountApproved() - capitalInstallment
+                    : amortizationSchedules.get(i - 2).getCapitalBalance() - capitalInstallment;
 
             if (i == numbersInstallments && capitalBalance != 0) {
                 capitalInstallment += capitalBalance;
                 capitalBalance = 0.00;
             }
-
-            var interestInstallment = calculateInterest(
-                    (i == 1) ? model.getAmountApproved() : amortizationSchedules.get(i - 2).getCapitalBalance(),
-                    model.getInterestRateLoan().getAnnualPercentage(),
-                    numbersInstallments,
-                    model.getFrequencyPayment()
-            );
 
             var amortizationSchedule = AmortizationSchedule.builder()
                     .loanId(model.getLoanId())
