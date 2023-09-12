a0cbde75a3c813f146bc253b1df4d11c1cc47961
Santiago Daglio <santidaglio@gmail.com>
2023-09-12 14:10:18 -0300

    implements mapper, usecase, dto, controller and loan application confirmation logic

--- backend/loans-application/src/main/java/s1014ftjavaangular/loansapplication/domain/mapper/LoanApplicationMapper.java
+++ backend/loans-application/src/main/java/s1014ftjavaangular/loansapplication/domain/mapper/LoanApplicationMapper.java
@@ -2,12 +2,14 @@ package s1014ftjavaangular.loansapplication.domain.mapper;
 
 import org.springframework.stereotype.Component;
 import s1014ftjavaangular.loansapplication.domain.model.dto.request.GeneralDataDto;
+import s1014ftjavaangular.loansapplication.domain.model.dto.response.ConfirmDataLoanApplicationDto;
 import s1014ftjavaangular.loansapplication.domain.model.entity.LoanApplication;
 import s1014ftjavaangular.loansapplication.domain.model.enums.Status;
 
 import java.time.LocalDate;
 import java.util.UUID;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 @Component
 public class LoanApplicationMapper {
@@ -16,9 +18,25 @@ public class LoanApplicationMapper {
             LoanApplication.builder()
                     .loanApplicationId(UUID.randomUUID().toString())
                     .customersUuid(dto.getCustomersUuid())
-                    .requestedAmount( dto.getRequestedAmount() )
-                    .createAt( LocalDate.now() )
+                    .requestedAmount(dto.getRequestedAmount())
+                    .createAt(LocalDate.now())
                     .status(Status.INCOMPLETE)
                     .loanApplicationNumber(loanApplicationNumber)
                     .build();
+
+
+    public final Function<LoanApplication, ConfirmDataLoanApplicationDto> modelToDto = (model) ->
+            ConfirmDataLoanApplicationDto.builder()
+                    .loanApplicationNumber(model.getLoanApplicationNumber())
+                    .requestedAmount(model.getRequestedAmount())
+                    .status(model.getStatus())
+                    .createAt(model.getCreateAt())
+                    .name(model.getGeneralData().getName())
+                    .lastname(model.getGeneralData().getLastname())
+                    .customersUuid(model.getCustomersUuid())
+                    .identification(model.getGeneralData().getIdentification())
+                    .phone(model.getGeneralData().getPhone())
+                    .build();
+
+
 }
